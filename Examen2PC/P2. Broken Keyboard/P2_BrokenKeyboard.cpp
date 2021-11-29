dbeddcd9c57fe59506aa91087ebc1ffc187d73c2
kpzaolod6000 <43651853+kpzaolod6000@users.noreply.github.com>
2021-11-29 08:36:26 -0500

    uploading exercise P2_BrokenKeyboard and test cases accepted

--- Examen2PC/P2. Broken Keyboard/P2_BrokenKeyboard.cpp
+++ Examen2PC/P2. Broken Keyboard/P2_BrokenKeyboard.cpp
@@ -2,6 +2,60 @@
 
 using namespace std;
 
+char text[1000010];
+
+int brokenKeyboard(int n_key){
+
+    int max_substring = 0;
+    int subS_cnt[128];
+    int start = 0,c_end = n_key-1;
+    int n_text = strlen(text);
+    //cout<<n_text<<endl;
+    int k_u = 0;
+    int cnt_aux = 0;
+    //int aux_m = 0;
+    
+    for (int i = 0; i < 128; ++i) subS_cnt[i] = 0;
+
+    //printf("%d\n", n_text);
+    for (int i = 0; i < n_key; ++i){
+        if(subS_cnt[text[i]] == 0) {k_u++;}
+        subS_cnt[text[i]]++;
+    }
+
+    
+    while(c_end < n_text-2){
+
+        while(k_u <= n_key && (c_end < n_text-2))
+        {
+            c_end++;   
+            if(subS_cnt[text[c_end]] == 0){
+                k_u++;
+            }
+            subS_cnt[text[c_end]]++;
+        }
+        
+        if(k_u > n_key){
+            start++;
+        }
+
+        
+        if((c_end - start + 1) > max_substring){
+            max_substring = c_end - start + 1;
+        }
+
+        
+        if(subS_cnt[text[start-1]] > 0){
+            subS_cnt[text[start-1]]--;
+            cnt_aux = subS_cnt[text[start-1]];
+            if(cnt_aux == 0){
+                k_u--;
+            }
+        }
+    }
+    return max_substring;
+}
+
 int main(){
 
     ios_base::sync_with_stdio(false);
@@ -9,69 +63,25 @@ int main(){
 
     while (true)
     {
-        
-        int n_key;
-        cin>>n_key;
-        
+      
+        scanf("%d", &n_key);
+        getchar();
 
-        if(n_key == 0) break;
-        else
-        {
+        if(n_key != 0){
             
-            char cin_text[1000010];
             cin.ignore();
-            if (fgets(cin_text, 1000010, stdin) != NULL){
-                string text = cin_text;
-                int max_substring = 0;
-            
-                vector<int> subS_cnt(128);
-                int start = 0,end = n_key-1;
-                int n_text = text.length();
-                int k_u = 0;
-                int cnt_aux = 0;
-                //int aux_m = 0;
-                
-                for (int i = 0; i < 128; ++i) subS_cnt[i] = 0;
-
-                for (int i = 0; i < n_key; ++i){
-                    if(subS_cnt[text[i]] == 0) {k_u++;}
-                    subS_cnt[text[i]]++;
-                }
-
-                
-                while(end < n_text-2){
-            
-                    while(k_u <= n_key && (end < n_text-2))
-                    {
-                        end++;   
-                        if(subS_cnt[text[end]] == 0){
-                            k_u++;
-                        }
-                        subS_cnt[text[end]]++;
-                    }
-                    
-                    if(k_u > n_key){
-                        start++;
-                    }
-
-                    
-                    if((end - start + 1) > max_substring){
-                        max_substring = end - start + 1;
-                    }
-
-                    
-                    if(subS_cnt[text[start-1]] > 0){
-                        subS_cnt[text[start-1]]--;
-                        cnt_aux = subS_cnt[text[start-1]];
-                        if(cnt_aux == 0){
-                            k_u--;
-                        }
-                    }
-                }
-                
-                printf("%d\n", max_substring);
+            int max_substring;
+            if (fgets(text, 1000010, stdin) != NULL){
+                //string text = cin_text;
+                max_substring = brokenKeyboard(n_key);
                 //break;
             }
+            cout<<max_substring<<endl;
+            //printf("%d\n", max_substring);
+        }
+        else
+        {
+            break;
         }
     }
         
